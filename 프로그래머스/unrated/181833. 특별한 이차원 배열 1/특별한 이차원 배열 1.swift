9c32ad21df23a2a9bc3c26ad380461f30563a04a
dongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-04 16:40:31 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.16 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n:Int) -> [[Int]] {
+    var answer = Array(repeating:(Array(repeating:0,count:n)),count : n)
+    for i in 0..<n{
+        for j in 0..<n{
+            if i == j{
+                answer[i][j] = 1
+            }
+        }
+    }
+    return answer
+}
